2cc38b939884faad06ebe5326eba3de809bd3d0f
Steven <90351981+StevenGrazius28@users.noreply.github.com>
2022-05-19 19:55:07 +0200

    Employee (19.5.2022)

---
+++ OrderingSystemDAL/EmployeeDao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderingSystemModel;
+
+namespace OrderingSystemDAL
+{
+    public class EmployeeDao : BaseDao
+    {
+        public List<Employee> GetAllAccounts()
+        {
+            string query = "SELECT username, [password] FROM [Employee]";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+        private List<Employee> ReadTables(DataTable dataTable)
+        {
+            List<Employee> employee = new List<Employee>();
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Employee employees = new Employee()
+                {
+                    userName = (string)dr["username"],
+                    password = (string)dr["password"],
+                };
+
+                employee.Add(employees);
+            }
+            return employee;
+        }
+    }
+}
---
+++ OrderingSystemLogic/EmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderingSystemModel;
+using OrderingSystemDAL;
+
+namespace OrderingSystemLogic
+{
+    public class EmployeeService
+    {
+        EmployeeDao employeedb;
+        public EmployeeService()
+        {
+            employeedb = new EmployeeDao();
+        }
+        public List<Employee> GetAccounts()
+        {
+            List<Employee> employees = employeedb.GetAllAccounts();
+            return employees;
+        }
+    }
+}
