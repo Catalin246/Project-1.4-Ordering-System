7516aec3bdc83b3e007c82cfe76b6c974d7e399e
Alessandra Ribeiro <689693@student.inholland.com>
2022-05-20 13:11:20 +0200

    entered bill model class and entities

---
+++ OrderingSystemModel/Bill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemModel
+{
+    public class Bill
+    {
+        public int BillID { get; set; }
+        public string PaymentType { get; set; }
+        public string BillNote { get; set; }
+        public Bill(int billId, string paymentType, string billNotes)
+        {
+            this.BillID = billId;
+            this.BillNote = billNotes;
+            this.PaymentType = paymentType;
+        }
+    }
+}
