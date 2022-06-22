8967078b4740d3a83f5aedc6ef94c5d4f45579c5
691136beril <90333992+691136beril@users.noreply.github.com>
2022-06-22 19:58:19 +0200

    corrected causes abou kitchenview (will be deleted)

--- OrderingSystemUI/Program.cs
+++ OrderingSystemUI/Program.cs
@@ -17,8 +17,8 @@ namespace OrderingSystemUI
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Login());
-            Application.Run(new TableView("Steven","Waiter"));
+            Application.Run(new Login());
+            //Application.Run(new TableView("Steven","Waiter"));
             //Application.Run(new TakeOrder());
         }
     }
